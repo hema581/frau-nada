228d3dbc0928e28c777a71a6aa618112a95ab7b8
hema581 <hema.mahmoud8000@gmail.com>
2025-08-25 08:03:34 +0300

    fix: use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+// تم تغيير الاستيراد من BrowserRouter إلى HashRouter
+import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* تم تغيير المكون إلى HashRouter (لا يحتاج basename) */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+);
