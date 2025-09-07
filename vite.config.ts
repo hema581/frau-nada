09deef8d7e649e6a5d442d5e6166ccdf1595c0d8
hema581 <hema.mahmoud8000@gmail.com>
2025-09-07 23:48:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // أضف هذا السطر
-  base: '/frau-nada/', 
+  base: './', // ✅ لازم تبقى كده عشان Netlify
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
